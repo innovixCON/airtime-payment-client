af58ea5a6da2bae5df733e2371d8e4402c534334
yubahwesc@gmail.com <yubahwesc@gmail.com>
2024-03-25 14:20:28 +0200

    dashbord

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,32 +1,65 @@
 export const DummyData = [
-    {
-      senderId: '1',
-      phoneNumber: '0784381529',
-      amountAirtime: '100',
-      receiverAirtimeNumber: '0780919691',
-    },
-    {
-      senderId: '2',
-      phoneNumber: '0784381530',
-      amountAirtime: '150',
-      receiverAirtimeNumber: '0786632361'
-    },
-    {
-        senderId: '3',
-        phoneNumber: '0784381531',
-        amountAirtime: '200',
-        receiverAirtimeNumber: '0786632362'
-      },
-      {
-        senderId: '4',
-        phoneNumber: '0784381530',
-        amountAirtime: '150',
-        receiverAirtimeNumber: '0786632361'
-      },
-      {
-        senderId: '5',
-        phoneNumber: '0784381530',
-        amountAirtime: '150',
-        receiverAirtimeNumber: '0786632361'
-      },
-  ];
+  {
+    senderId: "1",
+    phoneNumber: "0784381529",
+    amountAirtime: "100",
+    receiverAirtimeNumber: "0780919691",
+  },
+  {
+    senderId: "2",
+    phoneNumber: "0784381530",
+    amountAirtime: "150",
+    receiverAirtimeNumber: "0786632361",
+  },
+  {
+    senderId: "3",
+    phoneNumber: "0784381531",
+    amountAirtime: "200",
+    receiverAirtimeNumber: "0786632362",
+  },
+  {
+    senderId: "4",
+    phoneNumber: "0784381530",
+    amountAirtime: "150",
+    receiverAirtimeNumber: "0786632361",
+  },
+  {
+    senderId: "5",
+    phoneNumber: "0784381530",
+    amountAirtime: "150",
+    receiverAirtimeNumber: "0786632361",
+  },
+];
+
+export const userAmount = [
+  {
+    id: 1,
+    title: "Total Sent",
+    amount: 100,
+  },
+  {
+    id: 2,
+    title: "Total Received",
+    amount: 150,
+  },
+  {
+    id: 3,
+    title: "Total Airtime",
+    amount: 200,
+  },
+  {
+    id: 4,
+    title: "Total Airtime",
+    amount: 250,
+  },
+  {
+    id: 5,
+    title: "Total Airtime",
+    amount: 250,
+  },
+  {
+    id: 6,
+    title: "Total Airtime",
+    amount: 250,
+  },
+];
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,22 +1,24 @@
 import { ChatIcon } from "@heroicons/react/solid";
 import React, { useEffect } from "react";
-
+import { userAmount } from "../components/Data";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col grow bg-light mt-20">
+    <div className="flex flex-col grow bg-light mt-1">
       <div className="flex flex-row pb-8 justify-center">
         <div className="lg:ml-56 w-[90%] h-[100%]">
           <div className="grid grid-cols-2 mb-12 md:mb-24 lg:mb-0 lg:grid-cols-4">
-            <div className="border bg-white py-4 dark:bg-dark-bg md:w-[70%] h-32 md:h-40 mx-2 md:ml-16 lg:ml-10 mt-12 md:mt-20 lg:mt-8 text-center flex flex-col justify-center items-center rounded-lg">
-              <h4 className="font-['sans'] font-medium mt-2 text-base text-black">
-                Total Sent
-                {/* <ChatIcon className="text-primary w-10" /> */}
-              </h4>
-              <h1 className="font-['sans] font-semibold text-4xl mb-2 text-black">
-               $ 100 
-              </h1>
-            </div>
+            {userAmount.map((item, index) => (
+              <div key={index} className="border bg-white py-4 dark:bg-dark-bg md:w-[70%] h-32 md:h-40 mx-2 md:ml-16 lg:ml-10 mt-12 md:mt-20 lg:mt-8 text-center flex flex-col justify-center items-center rounded-lg">
+                <h4 className="font-['sans'] font-medium mt-2 text-base text-black">
+                  {item.title}
+                  {/* <ChatIcon className="text-primary w-10" /> */}
+                </h4>
+                <h1 className="font-['sans] font-semibold text-4xl mb-2 text-black">
+                  $ { item.amount }
+                </h1>
+              </div>
+            ))}
           </div>
         </div>
       </div>
